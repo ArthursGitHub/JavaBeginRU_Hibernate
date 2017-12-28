8f056098ec549a0c9fe0f4e3f30b0fa6cf69bf07
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-29 00:18:23 +0300

    0

---
+++ gradle/Lesson_11/src/main/java/ru/javabegin/training/hibernate/Start.java
@@ -0,0 +1,18 @@
+package ru.javabegin.training.hibernate;
+
+import org.jboss.logging.Logger;
+import ru.javabegin.training.hibernate.entity.Author;
+
+public class Start {
+    private static final Logger LOG = Logger.getLogger(AuthorHelper.class.getName());
+
+    public static void main(String[] args) {
+        // здесь не нужно открывать сессию - осталось от старого кода
+        //Session session = HibernateUtil.getSessionFactory().openSession();
+
+        Author author = new Author("тест3");
+        new AuthorHelper().addAuthor(author);
+
+        HibernateUtil.close();
+    }
+}
