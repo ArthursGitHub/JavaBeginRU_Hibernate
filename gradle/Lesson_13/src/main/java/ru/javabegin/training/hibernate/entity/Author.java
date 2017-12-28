e959ad82670f695981108b02851e619a9ecb13e0
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-29 01:29:49 +0300

    0

---
+++ gradle/Lesson_13/src/main/java/ru/javabegin/training/hibernate/entity/Author.java
@@ -0,0 +1,52 @@
+package ru.javabegin.training.hibernate.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
+public class Author implements Serializable{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // генерация ID через Autoincrement в MySQL
+    private long id;
+
+    private String name;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    public Author() {
+    }
+    public Author(String name){
+        this.name = name;
+    }
+    public Author(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+}
